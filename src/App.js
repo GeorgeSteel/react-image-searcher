aac5a325732c90efbc537a8d4b1b02035443a7df
Jorge <julianacero98@gmail.com>
2019-04-29 22:33:41 -0500

    loader added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import React, { Component } from 'react';
 import Searcher from './components/Searcher';
 import Header from './components/Header';
 import Result from './components/Result';
+import './spinner.css';
 
 export default class App extends Component {
   state = {
     term: '',
     imgs: [],
-    page: ''
+    page: '',
+    loading: false
   }
 
-  requestAPI = () => {
+  requestAPI = async () => {
     const term = this.state.term;
     const page = this.state.page;
     const url = `https://pixabay.com/api/?key=12345181-8c32532a40c8bb63db126ce43&q=${term}&per_page=30&page=${page}`;
 
-    fetch(url)
-        .then(resp => resp.json())
-        .then(data => this.setState({ imgs: data.hits }))
+    await fetch(url)
+          .then(resp => {
+            this.setState({ loading: true })
+            return resp.json()
+          })
+          .then(data => {
+            setTimeout(() => {
+              this.setState({ 
+                imgs: data.hits,
+                loading: false
+              });
+            }, 2000);
+          })
+          .catch(err => console.error(err));
   }
 
   dataSearch = term => {
@@ -62,6 +75,29 @@ export default class App extends Component {
   }
 
   render() {
+    const loading = this.state.loading;
+    let result;
+
+    if (loading) {
+      result = <div class="sk-cube-grid">
+                <div class="sk-cube sk-cube1"></div>
+                <div class="sk-cube sk-cube2"></div>
+                <div class="sk-cube sk-cube3"></div>
+                <div class="sk-cube sk-cube4"></div>
+                <div class="sk-cube sk-cube5"></div>
+                <div class="sk-cube sk-cube6"></div>
+                <div class="sk-cube sk-cube7"></div>
+                <div class="sk-cube sk-cube8"></div>
+                <div class="sk-cube sk-cube9"></div>
+              </div>;
+    } else {
+      result = <Result
+                imgs={ this.state.imgs }
+                nextPage={ this.nextPage }
+                previousPage={ this.previousPage }
+              />;
+    }
+
     return (
       <div className="app container">
         <div className="jumbotron">
@@ -72,11 +108,7 @@ export default class App extends Component {
         </div>
 
         <div className="row justify-content-center">
-          <Result
-            imgs={ this.state.imgs }
-            nextPage={ this.nextPage }
-            previousPage={ this.previousPage }
-          />
+          { result }
         </div>
 
       </div>
